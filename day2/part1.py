e1090d13080e3e0eb4b604c862572ae2bed7cd7b
ldearsley2 <ldearsley2@gmail.com>
2024-05-26 19:43:40 +0100

    Day 2, part 2

--- day2/part1.py
+++ day2/part1.py
@@ -28,6 +28,45 @@ def get_total(file):
 
     return total
 
-print(check_game("Game 10: 5 red, 1 green, 2 blue; 2 green, 8 blue, 6 red; 8 red, 3 blue, 2 green; 6 red, 1 green, 10 blue; 1 red, 10 blue"))
+#print(check_game("Game 10: 5 red, 1 green, 2 blue; 2 green, 8 blue, 6 red; 8 red, 3 blue, 2 green; 6 red, 1 green, 10 blue; 1 red, 10 blue"))
 
-print(get_total("input.txt"))
+#print(get_total("input.txt"))
+
+#Find the lowest values of red, green, blue in each game. Times them togetherand return the sum
+
+
+def get_min_values(game):
+    min = {
+            "red": None ,
+            "green": None,
+            "blue": None
+            }
+
+    game_id, rounds = game.split(":")
+    rounds = rounds.split(";")
+    rounds = [round.strip() for round in rounds]
+    for round in rounds:
+        cubes = round.split(",")
+        for cube in cubes:
+            count, colour = cube.split()
+            if min[colour] == None:
+                min[colour] = int(count)
+            if int(count) > min[colour]:
+                min[colour] = int(count)
+
+    total = min["red"] * min["green"] * min["blue"]
+    return total
+
+
+#print(get_min_values("Game 10: 5 red, 1 green, 2 blue; 2 green, 8 blue, 6 red; 8 red, 3 blue, 2 green; 6 red, 1 green, 10 blue; 1 red, 10 blue"))
+
+
+def total_min_powers(file):
+    total = 0
+    with open(file) as f:
+        for game in f:
+            total += get_min_values(game)
+
+    return total
+
+print(total_min_powers("input.txt"))
