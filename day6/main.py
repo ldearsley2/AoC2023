5f3ec6866bfbcdb498706e0acb6185991accf701
ldearsley2 <ldearsley2@gmail.com>
2024-06-11 19:54:43 +0100

    day6, part1

---
+++ day6/main.py
@@ -0,0 +1,34 @@
+def get_hold_times(distance: int, time: int):
+    total = 0
+    for i in range(time + 1):
+        hold_time = i
+        remaining_time = time - hold_time
+
+        if hold_time * remaining_time > distance:
+            total += 1
+
+    return total
+
+
+def main():
+    times = []
+    distances = []
+    with open("input.txt") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                times = line.split(":")[1].strip().split()
+            else:
+                distances = line.split(":")[1].strip().split()
+
+    total = 0
+
+    for i, distance in enumerate(distances):
+        if i == 0:
+            total += get_hold_times(int(distance), int(times[i]))
+        else:
+            total *= get_hold_times(int(distance), int(times[i]))
+
+    print(total)
+
+
+main()
